40c83c42adcac1997f965038b47bf66e131c8099
ZhangPeng <522567056@qq.com>
2022-12-13 20:06:00 +0800

    init commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  devServer: {
+    host: 'localhost',
+    port: 8999, // 端口号的配置
+    open: true // 自动打开浏览器
+  },
   transpileDependencies: true
 })
