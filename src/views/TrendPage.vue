a5f75fde91895ca053b5e864ecbd8cf8b125424e
ZhangPeng <522567056@qq.com>
2022-12-20 22:57:04 +0800

    feat: 销量趋势图表模块开发

---
+++ src/views/TrendPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="com-page">
+    <trend></trend>
+  </div>
+</template>
+
+<script>
+import Trend from '@/components/Trend'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    trend: Trend
+  }
+}
+</script>
+
+<style>
+
+</style>
